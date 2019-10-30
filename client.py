61cbac717015085a3ed3e2cd8408040391488fa2
tushar-1728 <tushar-1728@github.com>
2019-10-30 22:37:10 +0530

    checksum complete

--- client.py
+++ client.py
@@ -7,10 +7,12 @@ import socket
 serverSocket = 0
 clientSocket = 0
 
-def send_message(text):
+def send_message(text, var):
 	global clientSocket
-	msg = str(text.get("1.0", "end-1c"))
+	msg = str(text.get("1.0", "1.100"))
+	text.insert("1.0", "\n")
 	check_sum = checkSumClientSideImplementation(msg);
+	var.set(check_sum)
 	dict_msg_checksum ={
 		'msg': msg,
 		'check_sum':check_sum
@@ -24,59 +26,59 @@ def client():
 	clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 def add_binary_nums(x, y):
-    max_len = max(len(x), len(y))
+	max_len = max(len(x), len(y))
 
-    x = x.zfill(max_len)
-    y = y.zfill(max_len)
-    result = ''  # initialize the result
-    carry = 0   # initialize the carry
-    for i in range(max_len - 1, -1, -1):   # Traverse the string
-        r = carry
-        r += 1 if x[i] == '1' else 0
-        r += 1 if y[i] == '1' else 0
-        result = ('1' if r % 2 == 1 else '0') + result
-        carry = 0 if r < 2 else 1  # Compute the carry.
+	x = x.zfill(max_len)
+	y = y.zfill(max_len)
+	result = ''  # initialize the result
+	carry = 0   # initialize the carry
+	for i in range(max_len - 1, -1, -1):   # Traverse the string
+		r = carry
+		r += 1 if x[i] == '1' else 0
+		r += 1 if y[i] == '1' else 0
+		result = ('1' if r % 2 == 1 else '0') + result
+		carry = 0 if r < 2 else 1  # Compute the carry.
+	if carry != 0:
+		result = '1' + result
 
-    if carry != 0: result = '1' + result
-
-    return result.zfill(max_len)
+	return result.zfill(max_len)
 
 def checkSumClientSideImplementation(string):
-    sixteenBitArr = []
-    binString = ''.join(format(ord(x),'b') for x in string)
-    adtZero = math.ceil(len(binString)/16)* 16 - len(binString)
-    appZero = '0'*adtZero
-    binString = appZero + binString
-    print(binString,len(binString))
-    for i in range(0,len(binString),16):
-        sixteenBitArr.append(binString[i:i+16])
-    sixteenBitArr = map(lambda string:'0b'+string ,sixteenBitArr)
-       
-    sumOfBnos = '0b0'
-    for i in sixteenBitArr:
-         sumOfBnos = bin(int(sumOfBnos[2:], 2) + int(i[2:], 2))
+	sixteenBitArr = []
+	binString = ''.join(format(ord(x),'b') for x in string)
+	adtZero = math.ceil(len(binString)/16)* 16 - len(binString)
+	appZero = '0'*adtZero
+	binString = appZero + binString
+	print(binString,len(binString))
+	for i in range(0,len(binString),16):
+		sixteenBitArr.append(binString[i:i+16])
+	sixteenBitArr = map(lambda string:'0b'+string ,sixteenBitArr)
+	   
+	sumOfBnos = '0b0'
+	for i in sixteenBitArr:
+		 sumOfBnos = bin(int(sumOfBnos[2:], 2) + int(i[2:], 2))
 
-    if(len(sumOfBnos[2:])<16):
-        sumOfBnos = '0b' + str(0*(16 - len(sumOfBnos[2:]))) + sumOfBnos[2:]
+	if(len(sumOfBnos[2:])<16):
+		sumOfBnos = '0b' + str(0*(16 - len(sumOfBnos[2:]))) + sumOfBnos[2:]
 
-    print(sumOfBnos,2)
+	print(sumOfBnos,2)
 
-    temp = sumOfBnos[2:]
-    if(len(sumOfBnos[2:])>=17):
-        while(len(temp)!= 16):
-            temp = add_binary_nums(temp[1:],'0000000000000001')
-            #print(sumOfBnos,type(sumOfBnos))
-        #sumOfBnos = sumOfBnos[3:]
+	temp = sumOfBnos[2:]
+	if(len(sumOfBnos[2:])>=17):
+		while(len(temp)!= 16):
+			temp = add_binary_nums(temp[1:],'0000000000000001')
+			#print(sumOfBnos,type(sumOfBnos))
+		#sumOfBnos = sumOfBnos[3:]
 
-    comp = ""
-    for i in temp:
-        if(i=='1'):
-            comp = comp+'0'
-        else:
-            comp+='1'
-    print(comp)
+	comp = ""
+	for i in temp:
+		if(i=='1'):
+			comp = comp+'0'
+		else:
+			comp+='1'
+	print(comp)
 
-    return comp
+	return comp
 
 def checkSumServerSideImplementation(checksum,string):
 	sixteenBitArr = []
@@ -109,16 +111,28 @@ def checkSumServerSideImplementation(checksum,string):
 
 root = tk.Tk()
 root.title("Working of Checksum")
-root.geometry("400x400")
+root.geometry("600x600")
 
-text = tk.Text(root, bg = "grey", bd = 3)
+info = tk.Label(root, text = "This is the client side of checksum checking program.", font = "century 15")
+info.place(relx = 0.09, rely = 0.03)
+
+text = tk.Text(root, bg = "white", bd = 3)
 text.place(relwidth = 0.5, relheight = 0.3, relx = 0.25, rely = 0.25)
 
-c_client = tk.Button(root, text = "Create Client", command = client)
-c_client.place(relwidth = 0.2, relheight = 0.1, relx = 0.7, rely = 0.1)
+label = tk.Label(root, text = "Enter your\ntext here:", bd = 1, font = "verdana 10")
+label.place(relx = 0.07, rely = 0.375)
 
-c_sendMessage = tk.Button(root, text = "Send Message", command = lambda: send_message(text))
-c_sendMessage.place(relx = 0.375, rely = 0.6)
+c_client = tk.Button(root, text = "Create Client", command = client, font = "verdana 10")
+c_client.place(relwidth = 0.2, relheight = 0.075, relx = 0.4, rely = 0.1)
 
+label1 = tk.Label(root, text = "Checksum of sent message:", bd = 1, font = "verdana 10")
+label1.place(relx = 0.05, rely = 0.75)
+
+var = tk.StringVar()
+label2 = tk.Label(root, bg = "white", relief = tk.SUNKEN, textvariable=var, font = "verdana 10")
+label2.place(relwidth = 0.5, relx = 0.4, rely = 0.75)
+
+c_sendMessage = tk.Button(root, text = "Send Message", command = lambda: send_message(text, var))
+c_sendMessage.place(relwidth = 0.2, relheight = 0.075, relx = 0.375, rely = 0.6)
 
 root.mainloop()
--- server.py
+++ server.py
@@ -9,100 +9,109 @@ global serverSocket
 global clientSocket
 
 def add_binary_nums(x, y):
-    max_len = max(len(x), len(y))
+	max_len = max(len(x), len(y))
 
-    x = x.zfill(max_len)
-    y = y.zfill(max_len)
-    result = ''  # initialize the result
-    carry = 0   # initialize the carry
-    for i in range(max_len - 1, -1, -1):   # Traverse the string
-        r = carry
-        r += 1 if x[i] == '1' else 0
-        r += 1 if y[i] == '1' else 0
-        result = ('1' if r % 2 == 1 else '0') + result
-        carry = 0 if r < 2 else 1  # Compute the carry.
+	x = x.zfill(max_len)
+	y = y.zfill(max_len)
+	result = ''  # initialize the result
+	carry = 0   # initialize the carry
+	for i in range(max_len - 1, -1, -1):   # Traverse the string
+		r = carry
+		r += 1 if x[i] == '1' else 0
+		r += 1 if y[i] == '1' else 0
+		result = ('1' if r % 2 == 1 else '0') + result
+		carry = 0 if r < 2 else 1  # Compute the carry.
 
-    if carry != 0: result = '1' + result
+	if carry != 0: result = '1' + result
 
-    return result.zfill(max_len)
+	return result.zfill(max_len)
 
 def checkSumServerSideImplementation(checksum,string):
-    sixteenBitArr = []
-    binString = ''.join(format(ord(x),'b') for x in string)
-    adtZero = math.ceil(len(binString)/16)* 16 - len(binString)
-    appZero = '0'*adtZero
-    binString = appZero + binString
-    print(binString,len(binString))
-    for i in range(0,len(binString),16):
-        sixteenBitArr.append(binString[i:i+16])
-    sixteenBitArr = map(lambda string:'0b'+string ,sixteenBitArr)
-       
-    sumOfBnos = ""
-    for i in sixteenBitArr:
-        print(i)
-        sumOfBnos = add_binary_nums(sumOfBnos,i[2:])
-        print(sumOfBnos,len(sumOfBnos))
+	sixteenBitArr = []
+	binString = ''.join(format(ord(x),'b') for x in string)
+	adtZero = math.ceil(len(binString)/16)* 16 - len(binString)
+	appZero = '0'*adtZero
+	binString = appZero + binString
+	print(binString,len(binString))
+	for i in range(0,len(binString),16):
+		sixteenBitArr.append(binString[i:i+16])
+	sixteenBitArr = map(lambda string:'0b'+string ,sixteenBitArr)
+	   
+	sumOfBnos = ""
+	for i in sixteenBitArr:
+		print(i)
+		sumOfBnos = add_binary_nums(sumOfBnos,i[2:])
+		print(sumOfBnos,len(sumOfBnos))
 
 
-    if (len(sumOfBnos) >= 17):
-        while (len(sumOfBnos)!=16):
-            sumOfBnos = add_binary_nums(sumOfBnos[1:], '0000000000000001')
+	if (len(sumOfBnos) >= 17):
+		while (len(sumOfBnos)!=16):
+			sumOfBnos = add_binary_nums(sumOfBnos[1:], '0000000000000001')
 
-    print(checksum,sumOfBnos,len(sumOfBnos))
-    sumOfBnos = add_binary_nums(sumOfBnos,checksum)
-    print(checksum, sumOfBnos)
+	print(checksum,sumOfBnos,len(sumOfBnos))
+	sumOfBnos = add_binary_nums(sumOfBnos,checksum)
+	print(checksum, sumOfBnos)
 
-    for i in sumOfBnos:
-        if i=='0':
-            print("error")
-            return 0
-    print("no error")
-    return 1
+	for i in sumOfBnos:
+		if i=='0':
+			print("error")
+			return 0
+	print("no error")
+	return 1
 
-def recv_message(c_check_sum, text, serverSocket):
+def recv_message(serverSocket, text, var, var1):
 	data, clientAddress = serverSocket.recvfrom(2048)
 	data = dill.loads(data)
 	msg = data['msg']
 	check_sum = data['check_sum']
 	correctness = checkSumServerSideImplementation(check_sum, msg)
-	text.insert("1.0", msg+"\n")
+	text.insert(tk.INSERT, msg+"\n")
+	var.set(check_sum)
 	if(correctness):
-		c_check_sum.insert(0, "Checksum is correct")
+		var1.set("Correct")
 	else:
-		c_check_sum.insert(0, "Checksum is incorrect")
+		var1.set("Incorrect")
 	print(msg)
 	if(msg != "bye"):
-		recv_message(c_check_sum, text, serverSocket)
+		recv_message(serverSocket, text, var, var1)
 
 
-def server(c_check_sum, text):
+def server(text, var, var1):
 	serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	serverSocket.bind(('localhost', 12000))
-	t1 = threading.Thread(target = recv_message, args = [c_check_sum,text, serverSocket])
-	t1.start()
-	# t1.join()
-		
+	t1 = threading.Thread(target = recv_message, args = [serverSocket, text, var, var1])
+	t1.start()		
 
 
 root = tk.Tk()
 root.title("Working of Checksum")
 root.geometry("600x600")
 
+info = tk.Label(root, text = "This is the server side of checksum checking program.", font = "century 15")
+info.place(relx = 0.09, rely = 0.03)
 
-c_check_sum = tk.Entry(root, bg = "grey", bd = 3)
-text = tk.Text(root, bg = "grey", bd = 3)
-# action_arg = partial(action, text)
-c_server = tk.Button(root, text = "Create Server", command = lambda: server(c_check_sum, text), font = "Verdana 10")
-# c_sendMessage = tk.Button(root, text = "Send Message", command = lambda: action(text))
-text1 = """Message received
-from client"""
-label1 = tk.Label(root, text = text1, font = "Verdana 10")
+label1 = tk.Label(root, text = "Message received\nfrom client", font = "Verdana 10")
+label1.place(relx = 0.03, rely = 0.375)
 
+text = tk.Text(root, bg = "white", bd = 3)
+text.place(relwidth = 0.5, relheight = 0.3, relx = 0.25, rely = 0.25)
 
-c_server.place(relwidth = 0.17, relheight = 0.075, relx = 0.415, rely = 0.05)
-text.place(relwidth = 0.375, relheight = 0.225, relx = 0.3125, rely = 0.15)
-# c_sendMessage.place(relx = 0.375, rely = 0.6)
-c_check_sum.place(relx = 0, rely = 0.75, relwidth = 0.5)
-label1.place(relwidth = 0.26, relx = 0.025, rely = 0.23)
+label1 = tk.Label(root, text = "Checksum of received message:", bd = 1, font = "verdana 10")
+label1.place(relx = 0.03, rely = 0.65)
+
+var = tk.StringVar()
+label2 = tk.Label(root, bg = "white", relief = tk.SUNKEN, textvariable=var, font = "verdana 10")
+label2.place(relwidth = 0.5, relx = 0.4, rely = 0.65)
+
+label3 = tk.Label(root, text = "Correctness of checksum:", bd = 1, font = "verdana 10")
+label3.place(relx = 0.03, rely = 0.75)
+
+var1 = tk.StringVar()
+label4 = tk.Label(root, bg = "white", relief = tk.SUNKEN, textvariable=var1, font = "verdana 10")
+label4.place(relwidth = 0.5, relx = 0.4, rely = 0.75)
+
+c_server = tk.Button(root, text = "Create Server", command = lambda: server(text, var, var1), font = "Verdana 10")
+c_server.place(relwidth = 0.2, relheight = 0.075, relx = 0.4, rely = 0.1)
+
 
 root.mainloop()
